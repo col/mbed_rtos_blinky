783969058a2e036385dc322228201d0352e88e01
Colin Harris <col.w.harris@gmail.com>
2015-02-12 00:14:35 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "mbed.h"
+//#include "rtos.h"
+
+DigitalOut myled(LED1);
+
+int main() {
+    while(1) {
+        myled = 1;
+        wait(0.2);
+        myled = 0;
+        wait(0.2);
+    }
+}
